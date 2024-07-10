0bd82abd862a63037ec4a19ebee90899dc108255
Celebridge <gdmq_wqq@163.com>
2024-07-10 22:55:17 +0800

    实现渐进式rehash

---
+++ cache-core/src/main/java/com/github/houbb/cache/core/support/map/MyProgressiveReHashMap.java
@@ -0,0 +1,23 @@
+package com.github.houbb.cache.core.support.map;/**
+ * @Description
+ * @Author Celebridge
+ * @Date 2024/7/10
+ */
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @FileName MyProgressiveReHashMap
+ * @Description 渐进式rehash的hashmap
+ * @Author Celebridge
+ * @date 2024-07-10
+ **/
+public class MyProgressiveReHashMap<K,V> extends AbstractMap<K,V> implements Map<K,V> {
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+}
